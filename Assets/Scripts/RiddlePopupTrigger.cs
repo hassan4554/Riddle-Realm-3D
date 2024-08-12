412fba70894816ee6e0acda9924e941088a8b3c7
hassan4554 <hm9536057@gmail.com>
2024-08-12 15:55:30 +0500

    level 2

--- Assets/Scripts/RiddlePopupTrigger.cs
+++ Assets/Scripts/RiddlePopupTrigger.cs
@@ -1,49 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UIElements;
 
-public class RiddlePopupTrigger : MonoBehaviour
-{
-    public Animator riddleAnimator;
-    private bool showRiddle = false;
-    public GameObject riddle_image;
+//public class RiddlePopupTrigger : MonoBehaviour
+//{
+//    public Animator riddleAnimator;
+//    private bool showRiddle = false;
+//    public GameObject riddle_image;
 
-    private void Start()
-    {
-        Debug.Log("hidding Image");
-        HideImage();
-    }
+//    private void Start()
+//    {
+//        Debug.Log("hidding Image");
+//        HideImage();
+//    }
 
-    public void HideImage()
-    {
-        riddle_image.SetActive(false);
-    }
+//    public void HideImage()
+//    {
+//        riddle_image.SetActive(false);
+//    }
 
-    public void ShowImage()
-    {
-        riddle_image.SetActive(true);
-    }
+//    public void ShowImage()
+//    {
+//        riddle_image.SetActive(true);
+//    }
 
-    private void OnTriggerEnter(Collider other)
-    {
+//    private void OnTriggerEnter(Collider other)
+//    {
 
-        if (other.CompareTag("Player") && !showRiddle)
-        {
-            showRiddle = true;
-            Debug.Log("Showing Image");
-            ShowImage();
-            riddleAnimator.SetBool("isDisplayed", true);
-        }
+//        if (other.CompareTag("Player") && !showRiddle)
+//        {
+//            showRiddle = true;
+//            Debug.Log("Showing Image");
+//            ShowImage();
+//            riddleAnimator.SetBool("isDisplayed", true);
+//        }
 
-        //else
-        //{
-        //    showRiddle = false;
-        //    Debug.Log("Hiding Image 2");
-        //    HideImage();
-        //}
+//        //else
+//        //{
+//        //    showRiddle = false;
+//        //    Debug.Log("Hiding Image 2");
+//        //    HideImage();
+//        //}
 
 
-    }
+//    }
 
-}
+//}
